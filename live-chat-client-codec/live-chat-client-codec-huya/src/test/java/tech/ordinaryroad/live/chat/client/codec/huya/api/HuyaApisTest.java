d10d948a2b5cb259d4cf60a2436db25104fc9cc8
OrdinaryRoad <1962247851@qq.com>
2026-02-07 17:38:18 +0800

    test

--- live-chat-client-codec/live-chat-client-codec-huya/src/test/java/tech/ordinaryroad/live/chat/client/codec/huya/api/HuyaApisTest.java
+++ live-chat-client-codec/live-chat-client-codec-huya/src/test/java/tech/ordinaryroad/live/chat/client/codec/huya/api/HuyaApisTest.java
@@ -1,7 +1,9 @@
 package tech.ordinaryroad.live.chat.client.codec.huya.api;
 
 import cn.hutool.core.lang.Assert;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Test;
+import tech.ordinaryroad.live.chat.client.codec.huya.room.HuyaRoomInitResult;
 import tech.ordinaryroad.live.chat.client.commons.base.exception.BaseException;
 import tech.ordinaryroad.live.chat.client.commons.base.room.IRoomLiveStreamInfo;
 
@@ -14,6 +16,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @date 2023/10/1
  */
 class HuyaApisTest {
+
+    private static void checkIfEquals(HuyaRoomInitResult roomInitResult) {
+        long lp = roomInitResult.getTtProfileInfo().getLp();
+        JsonNode jsonNode = roomInitResult.getHyPlayerConfigStream().withArray("data").get(0);
+        long uid = jsonNode.get("gameLiveInfo").get("uid").asLong();
+        long liveChannel = jsonNode.get("gameLiveInfo").get("liveChannel").asLong();
+        long channel = jsonNode.get("gameLiveInfo").get("channel").asLong();
+        Assert.equals(lp, uid);
+        Assert.equals(lp, liveChannel);
+        Assert.equals(lp, channel);
+    }
 
     @Test
     void roomInit() {
@@ -40,5 +53,14 @@ class HuyaApisTest {
     void testRoomLiveStreamUrls() {
         List<IRoomLiveStreamInfo> roomLiveStreamUrls = HuyaApis.roomInit("lpl", null).getRoomLiveStreamUrls();
         Assert.notEmpty(roomLiveStreamUrls);
+    }
+
+    @Test
+    void testChannelId() {
+        checkIfEquals(HuyaApis.roomInit(333003, null));
+        checkIfEquals(HuyaApis.roomInit("bagea", null));
+        checkIfEquals(HuyaApis.roomInit("527988", null));
+        checkIfEquals(HuyaApis.roomInit(1995, null));
+        checkIfEquals(HuyaApis.roomInit(116, null));
     }
 }
